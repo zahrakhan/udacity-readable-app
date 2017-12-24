e06e38f6219652fcc7ebe6e37ed055fac2371d5e
ZahraKhan <zahra.khan@arbisoft.com>
2017-12-24 16:46:21 +0500

    chore: Project boostrapped from create-react-app
    Removed unnessary code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class ReadableApp extends Component {
+  render() {
+    return (
+      <div>Readable</div>
+    );
+  }
+}
+
+export default ReadableApp;

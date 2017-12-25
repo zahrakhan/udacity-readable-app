ee71441fee0a9a5b7ac5901cbbc29df402f4e765
ZahraKhan <zahra.khan@arbisoft.com>
2017-12-26 01:07:45 +0500

    feat: creating basic route

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
+import {Route, Switch} from 'react-router-dom'
 import '../App.css'
 
 class ReadableApp extends Component {
   render() {
     return (
-      <div>Readable</div>
+      <Switch>
+        <Route exact path="/" render={() => (
+          <div>readable</div>
+        )}/>
+      </Switch>
     )
   }
 }

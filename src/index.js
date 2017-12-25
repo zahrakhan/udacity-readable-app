48e2c5c94228ca2f0a9545b4296ccbb5ae121f64
ZahraKhan <zahra.khan@arbisoft.com>
2017-12-26 00:07:03 +0500

    feat: Installing and setting up BrowserRouter from react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import './index.css'
-import App from './components/App'
+import {BrowserRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
+import './index.css'
+import App from './components/App'
 
 const logger = store => next => action => {
   console.group(action.type)
@@ -18,18 +19,15 @@ const logger = store => next => action => {
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(
-    applyMiddleware(thunk),
-    applyMiddleware(logger)
-  )
-)
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk), applyMiddleware(logger)))
 
 ReactDOM.render(
-  <Provider store={store}>
-  <App/>
-</Provider>, document.getElementById('root'))
+  <BrowserRouter>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
 
 registerServiceWorker()

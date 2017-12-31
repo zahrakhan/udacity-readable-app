54ee77571240e39b6683800a6e35a8a7e92b7fd6
ZahraKhan <zahra.khan@arbisoft.com>
2017-12-31 23:14:32 +0500

    style: Added semantic ui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker'
+import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk';
+
 import rootReducer from './reducers'
-import './index.css'
 import App from './components/App'
+
+import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+
+import registerServiceWorker from './registerServiceWorker'
 
 const logger = store => next => action => {
   console.group(action.type)

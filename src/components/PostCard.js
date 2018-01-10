5a93fd53ba2218f1eee8aaa6456267e306addafb
ZahraKhan <zahra.khan@arbisoft.com>
2018-01-10 16:56:14 +0500

    fix: empty Post card  shown after delete

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -13,37 +13,44 @@ const PostCard = ({
   author,
   category,
   voteScore,
-  deleted,
+  deleted = true,
   commentCount
-}) => (
-  <Card color='teal'>
-    <Card.Content header={title} as={Link} to={'/' + category + '/' + id}/>
-    <Card.Content description={body}/>
-    <Card.Content extra>
-      <Item>
-        <Icon color='teal' name='user'></Icon>
-        {author}
-      </Item>
-      <Item>
-        <Icon color='teal' name='calendar'></Icon>
-        {moment(timestamp).fromNow()}
-      </Item>
-      <Divider/>
-      <Grid columns={3}>
-        <Grid.Column width={5}>
-          <Icon color='teal' name='tags'></Icon>
-          {category}
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <Icon color='teal' name='comments'></Icon>
-          {commentCount}
-        </Grid.Column>
-        <Grid.Column width={7}>
-          <ActionsOnPost postId={id}/>
-        </Grid.Column>
-      </Grid>
-    </Card.Content>
-  </Card>
-)
+}) => {
+  if (deleted)
+    return (
+      <span></span>
+    )
+  else
+    return (
+      <Card color='teal'>
+        <Card.Content header={title} as={Link} to={'/' + category + '/' + id}/>
+        <Card.Content description={body}/>
+        <Card.Content extra>
+          <Item>
+            <Icon color='teal' name='user'></Icon>
+            {author}
+          </Item>
+          <Item>
+            <Icon color='teal' name='calendar'></Icon>
+            {moment(timestamp).fromNow()}
+          </Item>
+          <Divider/>
+          <Grid columns={3}>
+            <Grid.Column width={5}>
+              <Icon color='teal' name='tags'></Icon>
+              {category}
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Icon color='teal' name='comments'></Icon>
+              {commentCount}
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <ActionsOnPost postId={id}/>
+            </Grid.Column>
+          </Grid>
+        </Card.Content>
+      </Card>
+    )
+}
 
 export default PostCard
